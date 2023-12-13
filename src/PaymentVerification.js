c63f301e12ee7107828bd1714a301cae8d91ea07
i.gichachi <iangichachi@gmail.com>
2023-12-13 14:36:37 +0300

    hdj

--- src/PaymentVerification.js
+++ src/PaymentVerification.js
@@ -1,11 +1,33 @@
-import React, { useState } from 'react';
-import './PaymentVerification.css'
+import React, { useState, useEffect } from 'react';
+import { useUserContext } from './UserContext'; // Import the context hook
+import './PaymentVerification.css';
 
 function PaymentVerification() {
     const [phoneNumber, setPhoneNumber] = useState('254');
     const [isProcessing, setIsProcessing] = useState(false);
+    const [isVerified, setIsVerified] = useState(false);
+    const { user } = useUserContext(); // Use the context to access user data
+
+    // Ensure the employerId is retrieved from the user context
+    const employerId = user ? user.userId : null;
+
+    const checkVerificationStatus = async () => {
+        if (!employerId) return;
+        const response = await fetch(`https://test-server-6mxa.onrender.com/payment-status/${employerId}`);
+        const data = await response.json();
+        if (data.verified) {
+            setIsProcessing(false);
+            setIsVerified(true);
+            alert('Payment verified successfully! Your account is now verified.');
+        }
+    };
 
     const initiatePayment = async () => {
+        if (!employerId) {
+            alert('Employer ID not found.');
+            return;
+        }
+
         setIsProcessing(true);
         const response = await fetch('https://test-server-6mxa.onrender.com/stk-push', {
             method: 'POST',
@@ -16,14 +38,22 @@ function PaymentVerification() {
         });
 
         const data = await response.json();
-        setIsProcessing(false);
 
         if (response.ok) {
             alert('Please check your phone to complete the M-Pesa transaction.');
+            const interval = setInterval(checkVerificationStatus, 5000); // Poll every 5 seconds
+            setTimeout(() => clearInterval(interval), 60000); // Stop polling after 60 seconds
         } else {
             alert('Error: ' + data.message);
+            setIsProcessing(false);
         }
     };
+
+    useEffect(() => {
+        if (isVerified) {
+            // Perform actions after verification, e.g., redirect or update UI
+        }
+    }, [isVerified]);
 
     return (
         <div className="payment-verification">
@@ -43,6 +73,7 @@ function PaymentVerification() {
                 </button>
             </div>
             {isProcessing && <p>Please complete the M-Pesa transaction on your phone...</p>}
+            {isVerified && <p>Your account has been verified!</p>}
         </div>
     );
 }
