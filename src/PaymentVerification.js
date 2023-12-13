872f3e5fb0a213feab42cae76695a8367dbe1050
i.gichachi <iangichachi@gmail.com>
2023-12-13 14:16:33 +0300

    finale

--- src/PaymentVerification.js
+++ src/PaymentVerification.js
@@ -2,30 +2,25 @@ import React, { useState } from 'react';
 import './PaymentVerification.css'
 
 function PaymentVerification() {
-    const [phoneNumber, setPhoneNumber] = useState('');
+    const [phoneNumber, setPhoneNumber] = useState('254');
     const [isProcessing, setIsProcessing] = useState(false);
 
     const initiatePayment = async () => {
-        // Prepend the country code if it's not already included
-        const fullPhoneNumber = phoneNumber.startsWith('254') ? phoneNumber : `254${phoneNumber}`;
-
         setIsProcessing(true);
         const response = await fetch('https://test-server-6mxa.onrender.com/stk-push', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({ phone_number: `+${fullPhoneNumber}`, amount: 1 })
+            body: JSON.stringify({ phone_number: phoneNumber, amount: 1 })
         });
 
         const data = await response.json();
         setIsProcessing(false);
 
         if (response.ok) {
-            // If the STK Push was initiated successfully, alert the user to complete the payment
             alert('Please check your phone to complete the M-Pesa transaction.');
         } else {
-            // If there was an error, display it to the user
             alert('Error: ' + data.message);
         }
     };
@@ -36,12 +31,14 @@ function PaymentVerification() {
             <div className="payment-form">
                 <input
                     type="tel"
-                    placeholder="Enter last 9 digits of your phone number"
+                    className="phone-input"
+                    placeholder="2547XXXXXXXX"
                     value={phoneNumber}
                     onChange={(e) => setPhoneNumber(e.target.value)}
                     disabled={isProcessing}
+                    maxLength={12}  // 3 digits for '254' and 9 for the phone number
                 />
-                <button onClick={initiatePayment} disabled={isProcessing || phoneNumber.length !== 9}>
+                <button onClick={initiatePayment} disabled={isProcessing || phoneNumber.length !== 12}>
                     {isProcessing ? 'Processing...' : 'Pay Ksh 1 with M-Pesa'}
                 </button>
             </div>
